281076e348078ea2aaf920980632bfaa7d78576a
eladlandman <eland.landman@intel.com>
2020-01-21 18:19:15 +0200

    Add project files.

---
+++ RealSense.Tools.DMSController/DMXLightController.cs
@@ -0,0 +1,124 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealSense.Tools.IVCam2.Validation.Main
+{
+    //Add option to read current light state so it won't be overridden by other lights settings
+    class DMXLightController : ILightController
+    {
+        const int PortNumber = 3;
+        SerialPort _serialPort;
+
+        public bool Init()
+        {
+            _serialPort = new SerialPort("COM" + PortNumber);
+
+            try
+            {
+                _serialPort.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logger.ApplicationLogger.Error($"Failed init DMX serial port: {PortNumber}", e);
+                return false;
+            }
+        }
+
+        public void TurnTesterLightOff(Logger logger)
+        {
+            logger.Info("Setting tester light off");
+            SendMessage(0, 0);
+        }
+
+        public void TurnTesterLightOn(Logger logger)
+        {
+            logger.Info("Setting tester light on");
+            SendMessage(0xff, 0xff);
+        }
+
+
+        public void SetStartupLights()
+        {
+            SetLights(false, true, true, false);
+        }
+
+        public void SetFailureLights()
+        {
+            SetLights(false, false, false, true);
+        }
+
+        public void SetQualityLights()
+        {
+            SetLights(false, false, true, true);
+        }
+
+        public void SetPassLights()
+        {
+            SetLights(false, true, false, false);
+        }
+
+        private void SetLights(bool internalLight, bool green, bool yellow, bool red)
+        {
+            byte internalLightsByte = internalLight ? (byte)0xff : (byte)0;
+            byte greenByte = green ? (byte)0xff : (byte)0;
+            byte yellowByte = yellow ? (byte)0xff : (byte)0;
+            byte redByte = red ? (byte)0xff : (byte)0;
+
+            SendMessage(internalLightsByte, internalLightsByte, greenByte, yellowByte, redByte);
+        }
+
+        /// <summary>
+        ///     bytes [0:1] - Internal tester lights
+        ///     bytes [2] - Green
+        ///     bytes [3] - Yellow
+        ///     bytes [4] - Red 
+        /// </summary>
+        /// <param name="data"></param>
+        private void SendMessage(params byte[] data)
+        {
+            var message = new byte[data.Length + 6];
+            message[0] = 0x7E;
+            message[1] = 0x06;
+            message[2] = (byte)((data.Length + 1) & 0xFF);
+            message[3] = (byte)((data.Length >> 8) & 0xFF);
+            message[4] = 0x00;
+
+            data.CopyTo(message, 5);
+            message[data.Length + 5] = 0xE7;
+
+            _serialPort.Write(message, 0, message.Length);
+        }
+    }
+
+    class MockLightController : ILightController
+    {
+        public bool Init()
+        {
+            return true;
+        }
+
+        public void SetFailureLights() { }
+
+        public void SetPassLights() { }
+
+        public void SetQualityLights() { }
+
+        public void SetStartupLights() { }
+
+        public void TurnTesterLightOff(Logger logger)
+        {
+            logger.Info("Mocking - turning tester light off");
+        }
+
+        public void TurnTesterLightOn(Logger logger)
+        {
+            logger.Info("Mocking - turning tester light on");
+        }
+    }
+}
---
+++ RealSense.Tools.DMXLightController/ILightController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IVCam.Tools.Infrastructure.Logging;
+using System.Threading.Tasks;
+using RealSense.Tools.ProjectorTester.Cmc;
+using System.Threading.Tasks;
+
+namespace RealSense.Tools.IVCam2.Validation.Main
+{
+    interface ILightController
+    {
+        void TurnTesterLightOff(Logger logger);
+        void TurnTesterLightOn(Logger logger);
+
+        void SetStartupLights();
+        void SetFailureLights();
+        void SetPassLights();
+        void SetQualityLights();
+        bool Init(Logger logger);
+    }
+}
